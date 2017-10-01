faba57f0790e9cfde1f4d560d464aead4dbba346
Muhammad Umer <myemailum14@gmail.com>
2017-10-01 04:01:37 -0400

    Create Question_B--Web_Crawler.js

---
+++ Part-1/Question_B--Web_Crawler.js
@@ -0,0 +1,19 @@
+// Solution:
+// Use a trie, a nested hash map where each map has keys of just one character.
+// It'd look like a tree
+// every node would have boolean value indicating if word completes there
+// Javascript has objects which act like hash map and can nest other objects, so nested hash maps
+
+var hash = {};
+
+
+function storeIntoNestedHash(word, node) {
+  let char = word[0];
+  node = node || hash;
+  node[char] = node[char] || {};
+  if (word.length == 1) {
+    node[char]['isWord'] = true;
+  } else {
+    storeIntoNestedHash(word.substr(1), node[char]);
+  }
+}
